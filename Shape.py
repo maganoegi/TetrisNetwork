c1ef0470b17e6443d3ead5af2901940f8b353147
maganoegi <platserg94@yahoo.com>
2019-11-19 01:06:48 +0100

    first commit

---
+++ Shape.py
@@ -0,0 +1,26 @@
+
+
+class Shape:
+    pass
+
+
+class I (Shape):        #   ====
+    pass
+
+class Г (Shape):        #   Г
+    pass
+
+class L (Shape):        #   L
+    pass
+
+class Square (Shape):   #   []
+    pass
+
+class Z (Shape):        #   Z
+    pass
+
+class S (Shape):        #   S
+    pass
+
+class T (Shape):        #   T
+    pass
